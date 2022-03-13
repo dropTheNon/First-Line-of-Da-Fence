b7dfbcc94bf97b4c73e837235b4b47a213a6ad5b
Josh McBroom <mcbroomjc@gmail.com>
2022-03-12 16:21:50 -0800

    auth routes tested and working

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.ORIGIN || "http://localhost:3000",
+    // origin: process.env.ORIGIN || "http://localhost:3000",
+    origin: true,
   })
 );
 
