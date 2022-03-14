0d25f0c3d008bb1adf4ab0009394624f402ed652
Josh McBroom <mcbroomjc@gmail.com>
2022-03-13 22:52:05 -0700

    all Leads routes working - CRUD complete for Leads

--- routes/leads.js
+++ routes/leads.js
@@ -11,15 +11,17 @@ const Lead = require("../models/Lead.model");
 // requiring our middleware to check user's level
 const levelCheck = require("../middleware/levelCheck");
 
+// GET all Leads
 router.get("/", levelCheck, (req, res, next) => {
     if (!req.user) {
         return res.status(403).json({ message: "Not logged in" });
     }
-    res.json({ message: "leads router working!" });
+    res.status(200).json({ message: "leads router working!" });
 
     // Create & call React form to let users view & create new leads
 });
 
+// POST route to CREATE new Lead
 router.post("/create", levelCheck, (req, res, next) => {
     Lead.create(req.body)
         .then((createdLead) => {
@@ -41,23 +43,89 @@ router.post("/create", levelCheck, (req, res, next) => {
                     { new: true }
                 )
                 .then((updatedLead) => {
-                    res.json({ 
+                    res.status(200).json({ 
                         user: updatedUser,
-                        "lead created": updateLead
+                        "lead created": updatedLead
                     });
                 })
                 .catch((err) => {
-                    res.json({ message: err });
+                    res.status(500).json({ message: err });
                 });
             })
             .catch((err) => {
-                res.json({ message: err });
+                res.status(500).json({ message: err });
             });
 
         })
         .catch((err) => {
-            res.json({ error: err });
+            res.status(500).json({ error: err });
         })
+});
+
+// GET individual Lead to READ
+router.get("/lead/:leadId", levelCheck, (req, res, next) => {
+    Lead.findById(req.params.leadId)
+        .then((leadFromDB) => {
+            res.status(200).json({ leadFromDB });
+            // Create React form to display Lead to User
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err, message: "Lead not found" });
+        });
 })
+
+// GET individual Lead to UPDATE
+router.get("/update/:leadId", levelCheck, (req, res, next) => {
+    Lead.findById(req.params.leadId)
+        .then((leadFromDB) => {
+            res.status(200).json({ leadFromDB });
+            // Display React form to update this Lead
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+})
+
+// POST route to UPDATE individual Lead
+router.post("/update/:leadId", levelCheck, (req, res, next) => {
+    Lead.findByIdAndUpdate(
+        req.params.leadId,
+        {
+            ...req.body,
+        }
+    )
+    .then((updatedLead) => {
+        res.status(200).json({ updatedLead });
+    })
+    .catch((err) => {
+        res.status(500).json({ message: err });
+    });
+});
+
+// POST route to DELETE individual Lead
+
+router.post("/delete/:leadId", levelCheck, (req, res, next) => {
+    User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $pull: { leads: req.params.leadId }
+        },
+        { new: true }
+    )
+    .then((updatedUser) => {
+        req.user = updatedUser;
+
+        Lead.findByIdAndDelete(req.params.leadId)
+            .then(() => {
+                res.status(200).json({ message: "Lead deleted!" });
+            })
+            .catch((err) => {
+                res.status(500).json({ message: err });
+            });
+    })
+    .catch((err) => {
+        res.status(500).json({ message: err });
+    });
+});
 
 module.exports = router;
